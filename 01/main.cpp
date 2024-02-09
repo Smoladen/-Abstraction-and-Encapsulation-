1313b0754e645df8d22f55a38f7a72f504117693
Denis Smal <smoladen2003@gmail.com>
2024-02-09 15:44:33 +0100

    Fixing mistakes in main.cpp

--- 01/main.cpp
+++ 01/main.cpp
@@ -2,54 +2,48 @@
 #include <fstream>
 #include <string>
 
-class Adress{
+class Address {
 
 private:
     std::string city;
     std::string street;
     int house;
     int apartment;
-public:
 
-    std::string getAdress(std::string city, std::string street, int house, int apartment){
-        this->city = city;
-        this->street = street;
-        this->house = house;
-        this->apartment = apartment;
+public:
+    Address() : city(""), street(""), house(0), apartment(0) {}
+    Address(std::string city, std::string street, int house, int apartment) :
+        city(city), street(street), house(house), apartment(apartment) {}
+
+    std::string getAddress() {
         return city + ", " + street + ", " + std::to_string(house) + ", " + std::to_string(apartment);
     }
-
 };
 
 int main() {
-    int numOfAdresses;
+    int numOfAddresses;
     std::string city;
     std::string street;
     int house;
     int apartment;
-    Adress adress;
 
-    std::ifstream read("/home/denis/Рабочий стол/Git Homework/Homework Abstraction and Encapsulation/01/in.txt");
-    std::ofstream write("/home/denis/Рабочий стол/Git Homework/Homework Abstraction and Encapsulation/01/out.txt");
-    read >> numOfAdresses;
+    std::ifstream read("in.txt");
+    std::ofstream write("out.txt");
+    read >> numOfAddresses;
 
-    std::string* arr = new std::string[numOfAdresses];
+    write << numOfAddresses;
 
-    write << numOfAdresses;
+    Address* addresses = new Address[numOfAddresses];
 
-    for(int i = 0; i < numOfAdresses; i++){
+    for(int i = 0; i < numOfAddresses; i++) {
         read >> city >> street >> house >> apartment;
-        arr[i] = adress.getAdress(city,street,house,apartment);
+        addresses[i] = Address(city, street, house, apartment);
     }
 
-    // for(int i = 0; i < numOfAdresses; i++){
-    //   write << arr[i] << std::endl;
-    // }
-
-    for(int i = numOfAdresses; i > 0; i--){
-        write << std::endl << arr[i-1];
+    for(int i = numOfAddresses - 1; i >= 0; i--) {
+        write << std::endl << addresses[i].getAddress();
     }
 
-    delete[] arr;
+    delete[] addresses;
     return 0;
 }
--- 02/main.cpp
+++ 02/main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <string>
 
-class Adress{
+class Address{
 
 private:
     std::string city;
@@ -10,27 +10,25 @@ private:
     int house;
     int apartment;
 public:
+    Address() : city(""), street(""), house(0), apartment(0) {}
+    Address(std::string city, std::string street, int house, int apartment) : city(city), street(street), house(house), apartment(apartment) {}
 
-    std::string getAdress(std::string city, std::string street, int house, int apartment){
-        this->city = city;
-        this->street = street;
-        this->house = house;
-        this->apartment = apartment;
+    std::string getAdress(){
         return city + ", " + street + ", " + std::to_string(house) + ", " + std::to_string(apartment);
     }
 
 };
 
-void sort(std::string* arr, int size){
-    int letter = 0;
+void sort(Address* arr, int size){
+    int k = 0;
     for(int i = 0; i < size - 1 ; i++){
         for (int j = i + 1; j < size; j++){
-            letter = 0;
-            if (arr[i] != arr[j]){
-                while (arr[i][letter] == arr[j][letter])
-                    letter++;
-                if (arr[i][letter] < arr[j][letter]) {
-                    std::string temp = arr[i];
+            k = 0;
+            if (arr[i].getAdress() != arr[j].getAdress()){
+                while (arr[i].getAdress()[k] == arr[j].getAdress()[k])
+                    k++;
+                if (arr[i].getAdress()[k] < arr[j].getAdress()[k]) {
+                    Address temp = arr[i];
                     arr[i] = arr[j];
                     arr[j] = temp;
                 }
@@ -45,27 +43,26 @@ int main() {
     std::string street;
     int house;
     int apartment;
-    Adress adress;
 
-    std::ifstream read("/home/denis/Рабочий стол/Git Homework/Homework Abstraction and Encapsulation/02/in.txt");
-    std::ofstream write("/home/denis/Рабочий стол/Git Homework/Homework Abstraction and Encapsulation/02/out.txt");
+    std::ifstream read("in.txt");
+    std::ofstream write("out.txt");
     read >> numOfAdresses;
 
-    std::string* arr = new std::string[numOfAdresses];
+    Address* adresses = new Address[numOfAdresses];
 
     write << numOfAdresses;
 
     for(int i = 0; i < numOfAdresses; i++){
         read >> city >> street >> house >> apartment;
-        arr[i] = adress.getAdress(city,street,house,apartment);
+        adresses[i] = Address(city,street,house,apartment);
     }
 
-    sort(arr, numOfAdresses);
+    sort(adresses, numOfAdresses);
 
-    for(int i = numOfAdresses; i > 0; i--){
-        write << std::endl << arr[i-1];
+    for(int i = numOfAdresses - 1; i >= 0; i--){
+        write << std::endl << adresses[i].getAdress();
     }
 
-    delete[] arr;
+    delete[] adresses;
     return 0;
 }
